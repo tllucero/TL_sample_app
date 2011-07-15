8347dd19102afd88e715b9583e659bae12e399ea
Thomas Lucero <tllucero@stardevsoft.com>
2011-07-15 14:23:45 -0400

    Added user microposts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'spork'
+#require 'factory_girl'
+#Factory.find_definitions
 
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
