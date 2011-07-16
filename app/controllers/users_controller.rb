7c0157956770ac3ab8ab9d76d0661f0e9ee8e0c2
Thomas Lucero <tllucero@stardevsoft.com>
2011-07-16 18:25:05 -0400

    Added user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_filter :authenticate, :only => [:index, :edit, :update]
+	before_filter :authenticate, :except => [:show, :new, :create]
 	before_filter :correct_user, :only => [:edit, :update]
 	before_filter :admin_user, :only => :destroy
 
@@ -53,11 +53,21 @@ class UsersController < ApplicationController
 		redirect_to users_path
 	end
 
-	private
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.following.paginate(:page => params[:page])
+		render 'show_follow'
+	end
 
-#		def authenticate
-#			deny_access unless signed_in?
-#		end
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(:page => params[:page])
+		render 'show_follow'
+	end
+
+	private
 
 		def correct_user
 			@user = User.find(params[:id])
